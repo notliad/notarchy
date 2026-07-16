e93b991ab6bf761b43d5f28c6574e31d322b866c
notliad <dailton.rcf@gmail.com>
2026-07-16 15:22:20 -0300

    fix installer

--- boot.sh
+++ boot.sh
@@ -3,9 +3,18 @@ set -euo pipefail
 
 repo_url="${NOTARCHY_REPO_URL:-https://github.com/notliad/notarchy.git}"
 repo_dir="${NOTARCHY_REPO_DIR:-/tmp/notarchy}"
+repo_branch="${NOTARCHY_BRANCH:-}"
 
-pacman -Sy --needed --noconfirm git
+if [[ "${1:-}" == "--branch" ]]; then
+  repo_branch="${2:-}"
+  shift 2
+fi
+
+pacman -Sy --needed --noconfirm git gum
 rm -rf "$repo_dir"
-git clone "$repo_url" "$repo_dir"
+if [[ -n "$repo_branch" ]]; then
+  git clone --branch "$repo_branch" "$repo_url" "$repo_dir"
+else
+  git clone "$repo_url" "$repo_dir"
+fi
 exec "$repo_dir/install/notarchy-install" "$@"
-
